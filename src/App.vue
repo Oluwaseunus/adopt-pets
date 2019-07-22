2f5c61edc172ef402ebacf48d3507ed222857fd8
Oluwaseunus <oluwaseunus@gmail.com>
2019-07-22 19:53:59 +0100

    Add pets pages and components

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,33 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Home &nbsp;</router-link>|
+      <router-link to="/cats">&nbsp; Cats &nbsp;</router-link>|
+      <router-link to="/dogs">&nbsp; Dogs &nbsp;</router-link>|
+      <router-link to="/pet">&nbsp; Pet &nbsp;</router-link>
     </div>
-    <router-view/>
+    <b-container>
+      <router-view />
+    </b-container>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
+  #app {
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
     color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+  }
+  #nav {
+    padding: 30px;
+    a {
+      font-weight: bold;
+      color: #2c3e50;
+      &.router-link-exact-active {
+        color: #42b983;
+      }
     }
   }
-}
 </style>
---
+++ src/data/dogs.js
@@ -0,0 +1,42 @@
+export default [
+  {
+    name: 'Sheeba',
+    breed: 'Collie',
+    gender: 'female',
+    age: 7,
+    color: 'black/white',
+    weight: 34,
+    location: 'fourside',
+    notes: 'Pure breed. Trained for competitions.'
+  },
+  {
+    name: 'Hillary',
+    breed: 'Mut',
+    gender: 'female',
+    age: 17,
+    color: 'orange/white',
+    weight: 37,
+    location: 'threed',
+    notes: 'super friendly'
+  },
+  {
+    name: 'Zeus',
+    breed: 'African hound',
+    gender: 'male',
+    age: 9,
+    color: 'gray',
+    weight: 68,
+    location: 'threed',
+    notes: 'super friendly'
+  },
+  {
+    name: 'Katie',
+    breed: 'Golden retriever',
+    gender: 'female',
+    age: 2,
+    color: 'black',
+    weight: 44,
+    location: 'threed',
+    notes: 'super friendly'
+  }
+];
